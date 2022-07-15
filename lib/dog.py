a60f7a9647a6ca2c6f020c4d2137e53bdaf64b72
ddeleon267 <dominique.a.deleon@gmail.com>
2022-07-15 14:58:32 -0400

    test

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Dog:
-    pass
+    def __init__(self, name=""):
+        self._name = ""
+        
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        # ipdb.set_trace()
+
+        if type(name) == "str" and len(name) > 0:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    
+    name = property(get_name, set_name)
